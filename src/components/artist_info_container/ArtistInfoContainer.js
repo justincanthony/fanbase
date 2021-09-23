7263074a385b57aa852ab74d83c7f6ae2810a6b8
Justin Anthony <justincanthony@gmail.com>
2021-09-23 09:58:02 -0600

    Display relevant artist ifo

--- src/components/artist_info_container/ArtistInfoContainer.js
+++ src/components/artist_info_container/ArtistInfoContainer.js
@@ -3,12 +3,24 @@ import { fetchArtist } from '../../apiCalls';
 import './ArtistInfoContainer.css';
 
 export const ArtistInfoContainer = ({ artistInfo }) => {
-	const { id, facebook_page_url, name, image_url, url } = artistInfo;
+  const { id, facebook_page_url, name, image_url, url } = artistInfo;
 
-	return (
-		<section className="artistInfo">
-			<h2>{name}</h2>
-			<img src={image_url} alt="Band" />
-		</section>
-	);
+  return (
+    <section className="artistInfoContainer">
+      <div className="bandImageWrapper">
+        <img src={image_url} alt="Band" />
+      </div>
+      <div className="artistDetails">
+        <h2 className="artistName">{name}</h2>
+        <ul className="artistLinks">
+          <li>
+            <a href={url}>bandsintown</a>
+          </li>
+          <li>
+            <a href={facebook_page_url}>FaceBook</a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 };
