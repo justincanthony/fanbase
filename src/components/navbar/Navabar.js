a3f34cec9ba528a36148456003ec88c9b942af5e
Justin Anthony <justincanthony@yahoo.com>
2021-09-22 22:38:14 -0600

    Add header element and link

--- src/components/navbar/Navabar.js
+++ src/components/navbar/Navabar.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 export const Navbar = () => {
-	return <div></div>;
+	return (
+		<header className="navbar">
+			<div className="logoWrapper">
+				<Link to="/">
+					<img src="" alt="App Logo" />
+				</Link>
+			</div>
+		</header>
+	);
 };
