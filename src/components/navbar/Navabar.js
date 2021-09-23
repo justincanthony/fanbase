3a461679ce665303e7caaf6ed48f170d25f2e939
Justin Anthony <justincanthony@yahoo.com>
2021-09-22 22:50:21 -0600

    Add html elements

--- src/components/navbar/Navabar.js
+++ src/components/navbar/Navabar.js
@@ -10,6 +10,10 @@ export const Navbar = () => {
 					<img src="" alt="App Logo" />
 				</Link>
 			</div>
+			<div className="searchBar">{/* <SearchBar /> */}</div>
+			<div className="BackButtonWrapper">
+				<Link to="/">Home/Back</Link>
+			</div>
 		</header>
 	);
 };
