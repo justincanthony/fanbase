86d1b9baa67b176e4d025a95e24f074a0d209f6e
Justin Anthony <justincanthony@yahoo.com>
2021-09-22 22:49:57 -0600

    Render navbar

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { Navbar } from '../navbar/Navabar';
 import './App.css';
 
 const App = () => {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<Navbar />
+		</div>
+	);
 };
 
 export default App;
