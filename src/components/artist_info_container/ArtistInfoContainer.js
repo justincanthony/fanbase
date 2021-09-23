4c8c029cffd1789233a8801b4d790d567847207c
Justin Anthony <justincanthony@yahoo.com>
2021-09-23 01:13:01 -0600

    Display artist info

--- src/components/artist_info_container/ArtistInfoContainer.js
+++ src/components/artist_info_container/ArtistInfoContainer.js
@@ -2,7 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { fetchArtist } from '../../apiCalls';
 import './ArtistInfoContainer.css';
 
-export const ArtistInfoContainer = () => {
-	
-	return <div></div>;
+export const ArtistInfoContainer = ({ artistInfo }) => {
+	const { id, facebook_page_url, name, image_url, url } = artistInfo;
+
+	return (
+		<section className="artistInfo">
+			<h2>{name}</h2>
+			<img src={image_url} alt="Band" />
+		</section>
+	);
 };
