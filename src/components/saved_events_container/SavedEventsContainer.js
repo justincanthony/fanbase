7d57c147faff114dc42bacf65a32c3cabd4c393a
Justin Anthony <justincanthony@gmail.com>
2021-09-24 13:04:10 -0600

    Create get from local stoarge function

--- src/components/saved_events_container/SavedEventsContainer.js
+++ src/components/saved_events_container/SavedEventsContainer.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './SavedEventsContainer.css';
 import { ConcertCard } from '../concert_card/ConcertCard';
 
-export const SavedEventsContainer = ({ savedConcerts }) => {
+export const SavedEventsContainer = () => {
+  const [savedConcerts, setSavedConcerts] = useState([]);
+
+  const getFromStorage = () => {
+    const keys = Object.keys(localStorage);
+    if (keys.length) {
+      keys.forEach((key) => {
+        setSavedConcerts([
+          ...savedConcerts,
+          JSON.parse(localStorage.getItem(key)),
+        ]);
+      });
+    }
+  };
+
+  useEffect(() => {
+    getFromStorage();
+  }, []);
+
   const savedEvents = savedConcerts.map((concert) => (
     <ConcertCard key={concert} concert={concert} />
   ));
-  return <div>{savedEvents}</div>;
+  return (
+    <section className="savedConcertContainer">
+      {/* {savedEvents < 1 && <Error message={`Lets Find Some Shows!`} />} */}
+      {savedEvents && savedEvents}
+    </section>
+  );
 };
