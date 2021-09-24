c6fa1bccabdb53d9c9ccc4b70ece3371b308e072
Justin Anthony <justincanthony@gmail.com>
2021-09-23 22:34:21 -0600

    Turn off live search

--- src/components/search_component/SearchComponent.js
+++ src/components/search_component/SearchComponent.js
@@ -2,22 +2,26 @@ import React, { useState, useEffect } from 'react';
 import './SearchComponent.css';
 
 export const SearchComponent = ({ handleChange }) => {
-	const [artist, setArtist] = useState('');
+  const [artist, setArtist] = useState('');
 
-	useEffect(() => {
-		handleChange(artist);
-	}, [artist]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleChange(artist);
+  };
 
-	return (
-		<div className="searchBar">
-			<form>
-				<input
-					type="text"
-					value={artist}
-					placeholder="artist name"
-					onChange={(e) => setArtist(e.target.value)}
-				></input>
-			</form>
-		</div>
-	);
+  return (
+    <div className="searchBar">
+      <form>
+        <input
+          type="text"
+          placeholder="artist name"
+          value={artist}
+          onChange={(e) => setArtist(e.target.value)}
+        />
+        <button type="submit" onClick={(e) => handleSubmit(e)}>
+          Search
+        </button>
+      </form>
+    </div>
+  );
 };
