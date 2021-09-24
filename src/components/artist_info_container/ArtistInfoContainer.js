7c224ee38edcfb8588200245165dc53c136fc197
Justin Anthony <justincanthony@gmail.com>
2021-09-23 20:50:01 -0600

    Remove unneeded imports

--- src/components/artist_info_container/ArtistInfoContainer.js
+++ src/components/artist_info_container/ArtistInfoContainer.js
@@ -1,14 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { fetchArtist } from '../../apiCalls';
+import React from 'react';
 import './ArtistInfoContainer.css';
 
 export const ArtistInfoContainer = ({ artistInfo }) => {
-	const { id, facebook_page_url, name, image_url, url } = artistInfo;
+  const { id, facebook_page_url, name, image_url, url } = artistInfo;
 
-	return (
-		<section className="artistInfo">
-			<h2>{name}</h2>
-			<img src={image_url} alt="Band" />
-		</section>
-	);
+  return (
+    <section className="artistInfo">
+      <h2>{name}</h2>
+      <img src={image_url} alt="Band" />
+    </section>
+  );
 };
