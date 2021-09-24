25910b2c9ab08eea654d8fad7e9c91581a8fceaa
Justin Anthony <justincanthony@gmail.com>
2021-09-24 08:29:15 -0600

    Add more event info to coard

--- src/components/concert_card/ConcertCard.js
+++ src/components/concert_card/ConcertCard.js
@@ -5,7 +5,13 @@ import { Link } from 'react-router-dom';
 export const ConcertCard = ({ concert }) => {
   return (
     <Link to="">
-      <article className="concertCard">{concert.venue.name}</article>
+      {console.log(concert)}
+      <article className="concertCard">
+        {concert.datetime}
+        {concert.venue.name}
+        {concert.title}
+        {concert.venue.location}
+      </article>
     </Link>
   );
 };
