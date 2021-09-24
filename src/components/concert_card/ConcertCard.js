4eb4c291dcc797c460ffd6a94a8a845d25cd5167
Justin Anthony <justincanthony@gmail.com>
2021-09-24 10:14:09 -0600

    Add function to set concert to local storage

--- src/components/concert_card/ConcertCard.js
+++ src/components/concert_card/ConcertCard.js
@@ -3,13 +3,24 @@ import './ConcertCard.css';
 import { Link } from 'react-router-dom';
 
 export const ConcertCard = ({ concert, handle }) => {
+  const handleClick = (concert) => {
+    localStorage.setItem(concert.id, JSON.stringify(concert));
+    console.log(concert);
+  };
+
   return (
-    <article className="concertCard" id={concert.id}>
+    <article className="concertCard" id={concert.id} concert={concert}>
       {concert.datetime}
       {concert.venue.name}
       {concert.title}
       {concert.venue.location}
-      {/* <button className="attendShow" type="submit" onClick={() =>  */}
+      <button
+        className="attendShow"
+        type="submit"
+        onClick={() => handleClick(concert)}
+      >
+        Save Concert
+      </button>
     </article>
   );
 };
