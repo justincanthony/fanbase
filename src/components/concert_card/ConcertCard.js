4b0d61e8d5dd4d65c5451dce9dc9d2f34512c128
Justin Anthony <justincanthony@gmail.com>
2021-09-24 15:11:55 -0600

    Remove console log

--- src/components/concert_card/ConcertCard.js
+++ src/components/concert_card/ConcertCard.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import './ConcertCard.css';
-import { Link } from 'react-router-dom';
 
 export const ConcertCard = ({ concert, handle }) => {
   const handleClick = (concert) => {
     localStorage.setItem(concert.id, JSON.stringify(concert));
-    console.log(concert);
   };
 
   return (
