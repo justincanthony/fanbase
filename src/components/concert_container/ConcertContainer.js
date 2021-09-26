f2e6d75d426e5d2f2f188d2fdbd666aa52cad9bf
Justin Anthony <justincanthony@gmail.com>
2021-09-26 13:43:50 -0600

    Add fetch call

--- src/components/concert_container/ConcertContainer.js
+++ src/components/concert_container/ConcertContainer.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './ConcertContainer.css';
+import { fetchEvents } from '../../apiCalls';
 import { ConcertCard } from '../concert_card/ConcertCard';
 
-export const ConcertContainer = ({ concerts }) => {
+export const ConcertContainer = ({ artist }) => {
+  const [concerts, setConcerts] = useState([]);
+  const [concertsError, setConcertsError] = useState('');
+
+  const getConcertInfo = (artist) => {
+    fetchEvents(artist)
+      .then((data) => setConcerts(data))
+      // .then((data) => console.log(data))
+      .catch((err) => setConcertsError(err.message));
+  };
+
+  useEffect(() => {
+    getConcertInfo(artist);
+  }, []);
+
   const concertCards = concerts.map((concert) => {
     return <ConcertCard key={concert.id} concert={concert} />;
   });
