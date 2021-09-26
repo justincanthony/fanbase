275f53858c7918f1e75b1cede6c920e169d6a720
Justin Anthony <justincanthony@gmail.com>
2021-09-26 13:44:25 -0600

    Remove unnecesary props

--- src/components/page/Page.js
+++ src/components/page/Page.js
@@ -1,34 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './Page.css';
 import { Navbar } from '../navbar/Navabar';
 import { ArtistInfoContainer } from '../artist_info_container/ArtistInfoContainer';
 // import { Footer }
 import { Route, Switch } from 'react-router-dom';
 import { WelcomeContainer } from '../welcome_container/WelcomeContainer';
-import { fetchArtist, fetchEvents } from '../../apiCalls';
 import { ConcertContainer } from '../concert_container/ConcertContainer';
 import { SavedEventsContainer } from '../saved_events_container/SavedEventsContainer';
 
 export const Page = () => {
-  const [artistInfo, setArtistInfo] = useState({});
-  const [concerts, setConcerts] = useState([]);
-  const [artistError, setArtistError] = useState('');
-  const [concertsError, setConcertsError] = useState('');
-
-  const handleChange = (artist) => {
-    fetchArtist(artist)
-      .then((data) => setArtistInfo(data))
-      // .then((data) => console.log(data))
-      .catch((err) => setArtistError(err.message));
-    fetchEvents(artist)
-      // .then((data) => console.log(data))
-      .then((data) => setConcerts(data))
-      .catch((err) => setConcertsError(err.message));
-  };
-
   return (
     <main className="main">
-      <Navbar handleChange={handleChange} />
+      <Navbar />
       <Switch>
         <Route exact path="/">
           <WelcomeContainer />
@@ -37,12 +20,11 @@ export const Page = () => {
           exact
           path="/artists/:artist"
           render={({ match }) => {
-            console.log(match);
             const { params } = match;
             return (
               <section className="pageContainer">
-                <ArtistInfoContainer artistInfo={artistInfo} />
-                <ConcertContainer concerts={concerts} />
+                <ArtistInfoContainer artist={params.artist} />
+                <ConcertContainer artist={params.artist} />
               </section>
             );
           }}
@@ -53,50 +35,3 @@ export const Page = () => {
     </main>
   );
 };
-
-// **************** Original Setup *******************
-
-// export const Page = () => {
-//   const [artistInfo, setArtistInfo] = useState({});
-//   const [concerts, setConcerts] = useState([]);
-//   const [artistError, setArtistError] = useState('');
-//   const [concertsError, setConcertsError] = useState('');
-
-//   const handleChange = (artist) => {
-//     fetchArtist(artist)
-//       .then((data) => setArtistInfo(data))
-//       // .then((data) => console.log(data))
-//       .catch((err) => setArtistError(err.message));
-//     fetchEvents(artist)
-//       // .then((data) => console.log(data))
-//       .then((data) => setConcerts(data))
-//       .catch((err) => setConcertsError(err.message));
-//   };
-
-//   return (
-//     <main className="main">
-//       <Navbar handleChange={handleChange} />
-//       <Switch>
-//         <Route exact path="/">
-//           <WelcomeContainer />
-//         </Route>
-//         <Route
-//           exact
-//           path="/artists/:artist"
-//           render={({ match }) => {
-//             console.log(match);
-//             const { params } = match;
-//             return (
-//               <section className="pageContainer">
-//                 <ArtistInfoContainer artistInfo={artistInfo} />
-//                 <ConcertContainer concerts={concerts} />
-//               </section>
-//             );
-//           }}
-//         ></Route>
-//         {/* <SavedEventsContainer /> */}
-//       </Switch>
-//       {/* <Footer /> */}
-//     </main>
-//   );
-// };
