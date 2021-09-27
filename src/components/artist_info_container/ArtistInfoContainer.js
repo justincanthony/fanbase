e5e91843e4a3dea27c072b1017b78e150490373e
Justin Anthony <justincanthony@gmail.com>
2021-09-26 18:55:14 -0600

    Import toast

--- src/components/artist_info_container/ArtistInfoContainer.js
+++ src/components/artist_info_container/ArtistInfoContainer.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { fetchArtist } from '../../apiCalls';
+import { ErrorMessage } from '../error_message/ErrorMessage';
 import './ArtistInfoContainer.css';
 
 export const ArtistInfoContainer = ({ artist }) => {
@@ -20,21 +21,26 @@ export const ArtistInfoContainer = ({ artist }) => {
   const { id, facebook_page_url, name, image_url, url } = artistInfo;
 
   return (
-    <section className="artistInfoContainer">
-      <div className="bandImageWrapper">
-        <img src={image_url} alt="Band" />
-      </div>
-      <div className="artistDetails">
-        <h2 className="artistName">{name}</h2>
-        <ul className="artistLinks">
-          <li>
-            <a href={url}>bandsintown</a>
-          </li>
-          <li>
-            <a href={facebook_page_url}>FaceBook</a>
-          </li>
-        </ul>
-      </div>
-    </section>
+    <>
+      {!artistInfo && <React.Fragment />}
+      {artistInfo && (
+        <section className="artistInfoContainer">
+          <div className="bandImageWrapper">
+            <img src={image_url} alt="Band" />
+          </div>
+          <div className="artistDetails">
+            <h2 className="artistName">{name}</h2>
+            <ul className="artistLinks">
+              <li>
+                <a href={url}>bandsintown</a>
+              </li>
+              <li>
+                <a href={facebook_page_url}>FaceBook</a>
+              </li>
+            </ul>
+          </div>
+        </section>
+      )}
+    </>
   );
 };
