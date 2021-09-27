5d767549ff8abf4580baacacac96aca6e83aaf3e
Justin Anthony <justincanthony@gmail.com>
2021-09-27 02:25:11 -0600

    Adjust format

--- src/components/concert_card/ConcertCard.js
+++ src/components/concert_card/ConcertCard.js
@@ -3,18 +3,18 @@ import dayjs from 'dayjs';
 import './ConcertCard.css';
 import { toast, ToastContainer } from 'react-toastify';
 
-export const ConcertCard = ({ concert }) => {
+export const ConcertCard = ({ concert, artist }) => {
   const handleClick = (concert) => {
     localStorage.setItem(concert.id, JSON.stringify(concert));
   };
 
   return (
     <article className="concertCard" id={concert.id} concert={concert}>
+      <p>{artist}</p>
       <p className="date">
         {dayjs(concert.datetime).format('MM/DD/YYYY   h:m')}
       </p>
       <p className="venue">{concert.venue.name}</p>
-      <p className="title">{concert.title}</p>
       <p className="location">{concert.venue.location}</p>
       <button
         className="attendShowButton"
