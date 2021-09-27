3bd8fb5774efbc8a997dc81d31c24306745ac604
Justin Anthony <justincanthony@gmail.com>
2021-09-27 02:24:41 -0600

    Adjust format

--- src/components/event_card/EventCard.js
+++ src/components/event_card/EventCard.js
@@ -3,13 +3,14 @@ import './EventCard.css';
 import dayjs from 'dayjs';
 
 export const EventCard = ({ concert, deleteShow }) => {
+  console.log(concert, 'here');
   return (
     <article className="eventCard" id={concert.id} concert={concert}>
+      <p className="lineUp">{concert.lineup[0]}</p>
       <p className="date">
-        {dayjs(concert.datetime).format('MM/DD/YYYY   hh:mm')}
+        {dayjs(concert.datetime).format('MM/DD/YYYY   h:mm')}
       </p>
       <p className="venue">{concert.venue.name}</p>
-      <p className="title">{concert.title}</p>
       <p className="location">{concert.venue.location}</p>
       <button
         className="deleteEventButton"
