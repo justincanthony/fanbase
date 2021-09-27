02234a1f30839457ba74693a5ef21ba7cdf682af
Justin Anthony <justincanthony@gmail.com>
2021-09-27 12:03:07 -0600

    Complete landing page test

--- cypress/integration/Landing_Page_spec.js
+++ cypress/integration/Landing_Page_spec.js
@@ -12,5 +12,35 @@ describe('Application Landing Page', () => {
   });
   it('Should be able to see elements on the page', () => {
     cy.get('h1').contains('FanBase');
+    cy.get('div.searchBar').find('input.searchInput');
+    cy.get('div.logoWrapper').contains('FanBase');
+    cy.get('section.welcomeContainer')
+      .get('h3')
+      .contains('Connecting Fans to Bands');
+  });
+  it('Should be able to capture value in the search input', () => {
+    cy.get('input.searchInput')
+      .type('Baroness')
+      .should('have.value', 'Baroness');
+  });
+  it('Should be able to navigate to a band page after searching for an artist', () => {
+    cy.intercept(
+      'GET',
+      'https://rest.bandsintown.com/artists/baroness/?app_id=888501139bcf6b4a81fec1c338f31325',
+      {
+        name: 'Baroness',
+        facebook_page_url: 'https://m.facebook.com/YourBaroness/?_rdr',
+        url: 'https://www.bandsintown.com/a/13813?came_from=267&app_id=888501139bcf6b4a81fec1c338f31325',
+        image_url: 'https://photos.bandsintown.com/large/9062513.jpeg',
+      }
+    );
+    cy.get('input.searchInput')
+      .type('Baroness')
+      .get('button.submitButton')
+      .click()
+      .url()
+      .should('include', 'artists/Baroness')
+      .get('section.artistInfoContainer')
+      .contains('Baroness');
   });
 });
