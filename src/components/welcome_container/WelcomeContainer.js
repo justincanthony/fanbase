93d426504fd0c1a397bc8ba28292919a954374aa
Justin Anthony <justincanthony@gmail.com>
2021-09-28 01:37:25 -0600

    Add div

--- src/components/welcome_container/WelcomeContainer.js
+++ src/components/welcome_container/WelcomeContainer.js
@@ -3,9 +3,11 @@ import './WelcomeContainer.css';
 export const WelcomeContainer = () => {
   return (
     <section className="welcomeContainer">
-      <h2>FanBase</h2>
-      <h3>Connecting Fans to Bands</h3>
-      <h4>Search for your favorite aritst to see where they will be next!</h4>
+      <div className="welcomeMessage">
+        <h2>FanBase</h2>
+        <h3>Connecting Fans to Bands</h3>
+        <h4>Search for your favorite aritst to see where they will be next!</h4>
+      </div>
     </section>
   );
 };
